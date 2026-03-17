648f3a261f6e28e18cfed3a4d1a16a3944e5a141
Linh Thuy Klatt <lklatt1@asu.edu>
2026-03-16 19:00:16 -0700

    Add board display and player status functionality

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,17 +1,16 @@
-public boolean makeMove(int row, int col) {
+public void displayBoard() {
+    System.out.println("Current Board:");
+    System.out.println("  1 2 3");
 
-    if (row < 0 || row >= 3 || col < 0 || col >= 3) {
-        return false;
+    for (int i = 0; i < 3; i++) {
+        System.out.print((i + 1) + " ");
+        for (int j = 0; j < 3; j++) {
+            System.out.print(board[i][j] + " ");
+        }
+        System.out.println();
     }
-
-    if (board[row][col] != '-') {
-        return false;
-    }
-
-    board[row][col] = currentPlayer;
-    return true;
 }
 
-public void switchPlayer() {
-    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+public char getCurrentPlayer() {
+    return currentPlayer;
 }
