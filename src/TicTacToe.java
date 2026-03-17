74a425f121285968eff82864e2a93afbf9af359e
Linh Thuy Klatt <lklatt1@asu.edu>
2026-03-16 17:47:38 -0700

    Improve startup initialization comments and messages

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,5 +2,7 @@ public class TicTacToe {
 	public static void main(String[] args){
 		System.out.println("Welcome to TicTacToe!");
 		System.out.printlm("Player X starts the game.");
+		System.out.println("Game setup complete.");
+		System.out.println("Ready to play!");
 	}
 }
